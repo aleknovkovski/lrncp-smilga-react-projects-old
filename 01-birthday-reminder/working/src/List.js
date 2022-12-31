e488bfcc7d0bd6426ab2ea9610f53f8dfa6523a9
Alek Novkovski <alek@greatcode2.com>
2022-12-31 22:45:26 +0100

    01: Map over data, create persons markup

--- 01-birthday-reminder/working/src/List.js
+++ 01-birthday-reminder/working/src/List.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 const List = (props) => {
   let data = props.data;
-  console.log(data);
+  // console.log(data);
+  const list = data.map((item, index) => {
+    return (
+      <div key={item.id} className='person'>
+        <img src={item.image} alt="avatar" />
+        <div>
+            <h4>{item.name}</h4>
+            <p>{item.age}</p>
+        </div>
+      </div>
+    )
+  })
   return (
     <>
-
+      <section className="list">
+        {list}
+      </section>
     </>
   );
 };
