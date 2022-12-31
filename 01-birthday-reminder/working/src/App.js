9463c3a6fb6a306dd43f6f18094153a687393c06
Alek Novkovski <alek@greatcode2.com>
2022-12-31 22:51:47 +0100

    01: Implement 'Clear All' functionality w useState

--- 01-birthday-reminder/working/src/App.js
+++ 01-birthday-reminder/working/src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+
+  const [list, setList] = useState(data);
+  function clearData(){setList([])}
+
   return <main>
     <section className="container">
       <h3>{`${data.length} birthdays today`}</h3>
-      <List data={data}/>
-      <button onClick={()=> console.log('you clicked me')}>Clear All</button>
+      <List data={list}/>
+      <button onClick={clearData}>Clear All</button>
     </section>
   </main>;
 }
