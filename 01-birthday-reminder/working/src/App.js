1bc6d0a9f6c656ecabf975673da121027916a5e5
Alek Novkovski <alek@greatcode2.com>
2022-12-31 22:22:15 +0100

    1: Setup overall skeleton of project 1

--- 01-birthday-reminder/working/src/App.js
+++ 01-birthday-reminder/working/src/App.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <main>
+    <section className="container">
+      <h3>0 birthdays</h3>
+      <List />
+      <button onClick={()=> console.log('you clicked me')}>Clear All</button>
+    </section>
+  </main>;
 }
 
 export default App;
