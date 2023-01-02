098af2cccb20df51f4304d59c7cf8a1974871e2f
Alek Novkovski <alek@greatcode2.com>
2023-01-02 23:34:11 +0100

    03: Create basis for fn that handles review change, pass down

--- 03-reviews/working/src/App.js
+++ 03-reviews/working/src/App.js
@@ -4,9 +4,19 @@ import data from "./data";
 
 function App() {
   const [currentReview, setCurrentReview] = useState(0)
+    function handleReviewChange(changeType) {
+        if(changeType==='prev') {
+            console.log('prev')
+        } else if(changeType==='next') {
+            console.log('next')
+        } else if(changeType==='random') {
+            console.log('random')
+        }
+    }
+
   return (
       <main>
-          <Review data={data[currentReview]}/>
+          <Review data={data[currentReview]} handleChange={handleReviewChange}/>
       </main>
   );
 }
