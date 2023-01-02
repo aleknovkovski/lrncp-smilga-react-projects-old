71b69544f8a55aa667aa0f0a0fcd38028cef26dc
Alek Novkovski <alek@greatcode2.com>
2023-01-02 17:06:29 +0100

    02: Fetch toursData from API

--- 02-tours/working/src/App.js
+++ 02-tours/working/src/App.js
@@ -6,6 +6,15 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [isLoading, setIsLoading] = useState(true)
+  async function fetchTours() {
+    const response = await fetch(url)
+    const toursData = await response.json()
+    console.log(toursData)
+  }
+
+  useEffect(()=> {
+    fetchTours()
+  },[])
 
   if(isLoading) {
     return <Loading />
