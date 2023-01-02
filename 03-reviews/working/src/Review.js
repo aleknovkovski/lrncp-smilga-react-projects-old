52af5905364707e85c1f9b7a6cf8b8359d056b78
Alek Novkovski <alek@greatcode2.com>
2023-01-02 23:17:38 +0100

    03: Add page title above review

--- 03-reviews/working/src/Review.js
+++ 03-reviews/working/src/Review.js
@@ -5,6 +5,10 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = (props) => {
   return (
       <section className='container'>
+        <div className='title'>
+            <h2>our reviews</h2>
+            <div className='underline'></div>
+        </div>
         <article className='review'>
             <h2>{props.data.name}</h2>
         </article>
