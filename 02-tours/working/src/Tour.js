4880f0ce5f1c73a8d45827a23b729004722ae47c
Alek Novkovski <alek@greatcode2.com>
2023-01-02 17:46:50 +0100

    02: Show image, and tour info

--- 02-tours/working/src/Tour.js
+++ 02-tours/working/src/Tour.js
@@ -3,9 +3,11 @@ import React, { useState } from 'react';
 const Tour = (props) => {
   return (
       <article className="single-tour">
-        <div className="tour-info">
-          <h4>{props.tourData.name}</h4>
-        </div>
+          <img src={props.tourData.image} alt={props.tourData.name}/>
+          <div className="tour-info">
+              <h4>{props.tourData.name}</h4>
+              <p>{props.tourData.info}</p>
+          </div>
       </article>
   );
 };
