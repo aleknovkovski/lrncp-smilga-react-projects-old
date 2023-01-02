8307d6e42336ea32fb33c1670272617a03c3e4c2
Alek Novkovski <alek@greatcode2.com>
2023-01-02 23:43:30 +0100

    03: Implement prev/next review changing

--- 03-reviews/working/src/App.js
+++ 03-reviews/working/src/App.js
@@ -6,9 +6,17 @@ function App() {
   const [currentReview, setCurrentReview] = useState(0)
     function handleReviewChange(changeType) {
         if(changeType==='prev') {
-            console.log('prev')
+            if(currentReview===0) {
+                setCurrentReview(data.length-1)
+            } else {
+                setCurrentReview((prev)=> prev-1)
+            }
         } else if(changeType==='next') {
-            console.log('next')
+            if(currentReview===data.length-1) {
+                setCurrentReview(0)
+            } else {
+                setCurrentReview((prev)=> prev+1)
+            }
         } else if(changeType==='random') {
             console.log('random')
         }
