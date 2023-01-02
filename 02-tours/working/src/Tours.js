ad91836579676480ddf4580390b18c6934fd1fae
Alek Novkovski <alek@greatcode2.com>
2023-01-02 19:20:22 +0100

    02: Show no-tours-left screen if user deletes all

--- 02-tours/working/src/Tours.js
+++ 02-tours/working/src/Tours.js
@@ -6,11 +6,20 @@ const Tours = (props) => {
     return <Tour key={tour.id} tourData={tour} removeHandler={props.removeHandler}/>
   });
 
+if(props.tours.length) {
   return (
       <div>
         {tourItems}
       </div>
   )
+} else {
+  return (
+      <div className='title'>
+        <h1>No Tours Left</h1>
+        <button className='btn'>refresh</button>
+      </div>
+  )
+}
 };
 
 export default Tours;
