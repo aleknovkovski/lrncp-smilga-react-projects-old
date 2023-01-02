cc0c2b34d4eed45748653a7e5ceafa9e4bfbbbfa
Alek Novkovski <alek@greatcode2.com>
2023-01-02 23:17:53 +0100

    03: Prettier reformat

--- 03-reviews/working/src/Review.js
+++ 03-reviews/working/src/Review.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
 
 const Review = (props) => {
-  return (
-      <section className='container'>
-        <div className='title'>
-            <h2>our reviews</h2>
-            <div className='underline'></div>
-        </div>
-        <article className='review'>
-            <h2>{props.data.name}</h2>
-        </article>
-      </section>
-  );
+    return (
+        <section className='container'>
+            <div className='title'>
+                <h2>our reviews</h2>
+                <div className='underline'></div>
+            </div>
+            <article className='review'>
+                <h2>{props.data.name}</h2>
+            </article>
+        </section>
+    );
 };
 
 export default Review;
