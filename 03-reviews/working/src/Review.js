22660900ef92753068b9419f1d272c48e3aab443
Alek Novkovski <alek@greatcode2.com>
2023-01-02 23:15:39 +0100

    03: Handle markup structure inside Review

--- 03-reviews/working/src/Review.js
+++ 03-reviews/working/src/Review.js
@@ -3,7 +3,13 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = (props) => {
-  return <h2>{props.data.name}</h2>;
+  return (
+      <section className='container'>
+        <article className='review'>
+            <h2>{props.data.name}</h2>
+        </article>
+      </section>
+  );
 };
 
 export default Review;
