9af74bdc8055f6503d9c582328c6dd895f48cf19
Alek Novkovski <alek@greatcode2.com>
2023-01-03 19:38:15 +0100

    04: Make showing of infoArea conditional on state

--- 04-accordion/working/src/Question.js
+++ 04-accordion/working/src/Question.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({question: {info, title}}) => {
+    const [opened, setOpened] = useState(false)
+    const infoArea = opened ? <p>{info}</p> : null
+
   return (
       <article className="question">
         <header>
           <h4>{title}</h4>
           <button className="btn"><AiOutlinePlus /></button>
         </header>
-          <p>{info}</p>
+          {infoArea}
       </article>
   );
 };
