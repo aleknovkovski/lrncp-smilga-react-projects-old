9cd5c60558b1e0f9db2dd895012b4492caa2e3b3
Alek Novkovski <alek@greatcode2.com>
2023-01-03 13:40:21 +0100

    03: Refactor and fix randomizer logic

--- 03-reviews/working/src/App.js
+++ 03-reviews/working/src/App.js
@@ -4,26 +4,32 @@ import data from "./data";
 
 function App() {
   const [currentReview, setCurrentReview] = useState(0)
+    function validatedIndex(indexToCheck) {
+      if(indexToCheck < 0) {
+          return data.length-1
+      }
+      if (indexToCheck > data.length-1) {
+          return 0
+      }
+      return indexToCheck
+    }
     function handleReviewChange(changeType) {
         if(changeType==='prev') {
-            if(currentReview===0) {
-                setCurrentReview(data.length-1)
-            } else {
-                setCurrentReview((prev)=> prev-1)
-            }
-        } else if(changeType==='next') {
-            if(currentReview===data.length-1) {
-                setCurrentReview(0)
-            } else {
-                setCurrentReview((prev)=> prev+1)
-            }
-        } else if(changeType==='random') {
+                setCurrentReview((prev)=> validatedIndex(prev-1))
+        }
+        if(changeType==='next') {
+            setCurrentReview((prev)=> validatedIndex(prev+1))
+        }
+
+        if(changeType==='random') {
             const random = Math.floor(Math.random() * data.length);
             console.log(random)
             if (currentReview===random) {
-                console.log('current review same as random')
+                console.log('current review is same')
+                setCurrentReview(()=> validatedIndex(random+1))
+            } else {
+                setCurrentReview(random)
             }
-            setCurrentReview(random)
         }
     }
 
