43a1c59d7d0872b941195506e76296d5d2e9c07a
Alek Novkovski <alek@greatcode2.com>
2023-01-03 19:46:16 +0100

    04: Pass a handleToggle fn to singleQuestion

--- 04-accordion/working/src/App.js
+++ 04-accordion/working/src/App.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 
-const questionsList = data.map((question)=> {
-  return (
-      <SingleQuestion question={question} key={question.id}/>
-  )
-})
-
 function App() {
+
+    function handleToggle(id) {
+        console.log('need to toggle id of ' + id)
+    }
+
+    const questionsList = data.map((question)=> {
+        return (
+            <SingleQuestion question={question} key={question.id} handleToggle={handleToggle}/>
+        )
+    })
+
   return (
       <div className="container">
         <h3>questions and answers about login</h3>
