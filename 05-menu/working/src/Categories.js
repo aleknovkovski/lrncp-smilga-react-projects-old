ab6b0bc07407b95b3f2b13992139bb049f6c706d
Alek Novkovski <alek@greatcode2.com>
2023-01-04 13:46:23 +0100

    05: Pass category into data attribute for each menu item

--- 05-menu/working/src/Categories.js
+++ 05-menu/working/src/Categories.js
@@ -6,7 +6,9 @@ const Categories = ({handleClick, categories}) => {
   return (<div className='btn-container' onClick={handleClick}>
     {
       categories.map((category)=> {
-        return <button className="filter-btn">{category}</button>
+        return <button className="filter-btn" data-category={category}>
+          {category}
+        </button>
       })
     }
   </div>);
