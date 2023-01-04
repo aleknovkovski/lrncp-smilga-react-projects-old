8a79607e026f7cb6ce1894883d6d6c899fa284ef
Alek Novkovski <alek@greatcode2.com>
2023-01-04 10:24:18 +0100

    05: Finish Menu markup

--- 05-menu/working/src/Menu.js
+++ 05-menu/working/src/Menu.js
@@ -4,7 +4,18 @@ const Menu = ({items}) => {
   return (<div className="section-center">
     {
       items.map((item)=> {
-        return <div>{item.title}</div>
+        return <article className="menu-item">
+          <img src={item.img} alt="" className="photo" />
+          <div className="item-info">
+            <header>
+              <h4>{item.title}</h4>
+              <h4 className="price">{"$" + item.price}</h4>
+            </header>
+            <p className="item-text">
+              {item.desc}
+            </p>
+          </div>
+        </article>
       })
     }
   </div>);
