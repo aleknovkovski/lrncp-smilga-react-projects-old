8650189d8aa78fa77c7fbdb09eb340ca3a7f9522
Alek Novkovski <alek@greatcode2.com>
2023-01-04 20:49:55 +0100

    07: Prettier reformat

--- 07-slider/working/src/App.js
+++ 07-slider/working/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
+import React, {useState, useEffect} from 'react';
+import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
+import {FaQuoteRight} from 'react-icons/fa';
 import data from './data';
+
 function App() {
-  return (
-      <section className="section">
-        <div className="title">
-            <h2><span>/</span>reviews</h2>
-        </div>
-          <div className="section-center">
-              <button className="prev">{"<<"}</button>
-              <button className="next">{">>"}</button>
-          </div>
-      </section>
-  );
+    return (
+        <section className="section">
+            <div className="title">
+                <h2><span>/</span>reviews</h2>
+            </div>
+            <div className="section-center">
+                <button className="prev">{"<<"}</button>
+                <button className="next">{">>"}</button>
+            </div>
+        </section>
+    );
 }
 
 export default App;
