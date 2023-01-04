4f9aff77b197bd39cf790353801095b890a35206
Alek Novkovski <alek@greatcode2.com>
2023-01-04 20:49:08 +0100

    07: Setup basic markup

--- 07-slider/working/src/App.js
+++ 07-slider/working/src/App.js
@@ -3,7 +3,17 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  return (
+      <section className="section">
+        <div className="title">
+            <h2><span>/</span>reviews</h2>
+        </div>
+          <div className="section-center">
+              <button className="prev">{"<<"}</button>
+              <button className="next">{">>"}</button>
+          </div>
+      </section>
+  );
 }
 
 export default App;
