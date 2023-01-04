3fc16bad12ab62d8b01f20e13bee900f61f97f9f
Alek Novkovski <alek@greatcode2.com>
2023-01-04 21:14:25 +0100

    07: Add test slides markup, uncomment slide animation CSS

--- 07-slider/working/src/App.js
+++ 07-slider/working/src/App.js
@@ -4,12 +4,21 @@ import {FaQuoteRight} from 'react-icons/fa';
 import data from './data';
 
 function App() {
+
+    const slidesMarkup = (<>
+        <article className="lastSlide"><p>aaa</p></article>
+        <article className="activeSlide"><p>bbb</p></article>
+        <article className="nextSlide"><p>ccc</p></article>
+        <article className="nextSlide"><p>ddd</p></article>
+    </>)
+
     return (
         <section className="section">
             <div className="title">
                 <h2><span>/</span>reviews</h2>
             </div>
             <div className="section-center">
+                {slidesMarkup}
                 <button className="prev">{"<<"}</button>
                 <button className="next">{">>"}</button>
             </div>
