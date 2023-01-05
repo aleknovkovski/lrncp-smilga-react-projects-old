1547e4a2130b0e8dc030d42f9939d99c69919ff0
Alek Novkovski <alek@greatcode2.com>
2023-01-05 19:32:11 +0100

    08: Set paragraphs on submit

--- 08-lorem-ipsum/working/src/App.js
+++ 08-lorem-ipsum/working/src/App.js
@@ -4,16 +4,17 @@ function App() {
   const [paragraphs, setParagraphs] = useState([])
   const [number, setNumber] = useState(0)
 
-  useEffect(()=> {
+  const submitHandler = (e) => {
+    e.preventDefault()
     if (number > 0) {
       setParagraphs(data.slice(0, number))
     }
-  })
+  }
   
   return (
   <section className="section-center">
     <h3>tired of boring lorem ipsum</h3>
-    <form className="lorem-form">
+    <form className="lorem-form" onSubmit={submitHandler}>
       <label htmlFor="amount">paragraphs:</label>
       <input type="number" name="amount" id="amount" value={number} onChange={(e)=> setNumber(e.target.value)}/>
       <button className="btn">generate</button>
