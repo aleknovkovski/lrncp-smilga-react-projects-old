3fba110990fc679301e486991287e48536d352b3
Alek Novkovski <alek@greatcode2.com>
2023-01-05 19:15:46 +0100

    08: Setup markup for app

--- 08-lorem-ipsum/working/src/App.js
+++ 08-lorem-ipsum/working/src/App.js
@@ -4,7 +4,15 @@ function App() {
   
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className="section-center">
+    <h3>tired of boring lorem ipsum</h3>
+    <form className="lorem-form">
+      <label htmlFor="amount">paragraphs:</label>
+      <input type="number" name="amount" id="amount" value="0"/>
+      <button className="btn">generate</button>
+    </form>
+    <article className="lorem-text"></article>
+  </section>
     )
 }
 
