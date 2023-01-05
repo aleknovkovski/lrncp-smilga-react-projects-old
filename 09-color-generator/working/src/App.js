577f8127b4177774dc12a00c2ab0c4a1de5381bc
Alek Novkovski <alek@greatcode2.com>
2023-01-05 20:51:15 +0100

    09: Setup colors state var w defaultValues

--- 09-color-generator/working/src/App.js
+++ 09-color-generator/working/src/App.js
@@ -6,6 +6,9 @@ import Values from 'values.js'
 function App() {
   const defaultValues = new Values('#f15025')
   console.log(defaultValues.all(10))
+
+  const [colors, setColors] = useState(defaultValues.all(10))
+
   return (<>
     <section className="container">
       <h3>color generator</h3>
