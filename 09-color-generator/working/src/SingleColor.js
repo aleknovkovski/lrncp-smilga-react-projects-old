8408e355a457665c22d09f7b3abd4d0bccec8a65
Alek Novkovski <alek@greatcode2.com>
2023-01-05 21:06:10 +0100

    09: Build out SingleColor component

--- 09-color-generator/working/src/SingleColor.js
+++ 09-color-generator/working/src/SingleColor.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({type, weight, rgb}) => {
+    const bgColor = {backgroundColor: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`}
+    return (
+      <article className="color" style={bgColor}>
+          <p className="percent-value">{weight}%</p>
+          <p className="color-value">{rgbToHex(rgb[0], rgb[1], rgb[2])}</p>
+      </article>
+  )
 }
 
 export default SingleColor
