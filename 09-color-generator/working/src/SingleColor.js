a7f8db4fce05d48eec090114fb1c2539d90fb87c
Alek Novkovski <alek@greatcode2.com>
2023-01-06 00:16:07 +0100

    09: Build copy to clipboard function

--- 09-color-generator/working/src/SingleColor.js
+++ 09-color-generator/working/src/SingleColor.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
+async function copyToClipboard(color) {
+    try {
+        await navigator.clipboard.writeText(color);
+        console.log('Content copied to clipboard');
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
 
 const SingleColor = ({color: {type, weight, rgb}}) => {
     const bgColor = {backgroundColor: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`}
     const colorClass = `color ${type==="shade" ? "color-light" : false }`
     return (
-      <article className={colorClass} style={bgColor}>
+      <article className={colorClass} style={bgColor} onClick={()=> copyToClipboard("copied text")}>
           <p className="percent-value">{weight}%</p>
           <p className="color-value">{rgbToHex(rgb[0], rgb[1], rgb[2])}</p>
       </article>
