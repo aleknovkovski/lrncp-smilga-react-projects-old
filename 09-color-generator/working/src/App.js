27c1c589ad94267501380b1f67570051ea313cd8
Alek Novkovski <alek@greatcode2.com>
2023-01-05 20:21:08 +0100

    09: Setup basic markup structure

--- 09-color-generator/working/src/App.js
+++ 09-color-generator/working/src/App.js
@@ -4,7 +4,16 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  return (<>
+    <section className="container">
+      <h3>color generator</h3>
+      <form>
+        <input type="text" placeholder="#f15025"/>
+        <button type="submit" className="btn">submit</button>
+      </form>
+    </section>
+    <section className="colors"></section>
+  </>)
 }
 
 export default App
