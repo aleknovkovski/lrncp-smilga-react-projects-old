d7bf820b1ffe2f5916f3da4258c3d3251d06b1ac
Alek Novkovski <alek@greatcode2.com>
2023-01-06 14:09:04 +0100

    10: Prettier reformat

--- 10-grocery-bud/working/src/App.js
+++ 10-grocery-bud/working/src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return (
-      <section className="section-center">
-        <form className="grocery-form">
-          <h3>grocery bud</h3>
-          <div className="form-control">
-              <input type="text" className="grocery" placeholder="e.g. eggs"/>
-              <button type="submit" className="submit-btn">submit</button>
-          </div>
-        </form>
-        <div className="grocery-container">
-            <List items={["a", "b", "c"]}/>
-        </div>
-      </section>
-  )
+    return (
+        <section className="section-center">
+            <form className="grocery-form">
+                <h3>grocery bud</h3>
+                <div className="form-control">
+                    <input type="text" className="grocery" placeholder="e.g. eggs"/>
+                    <button type="submit" className="submit-btn">submit</button>
+                </div>
+            </form>
+            <div className="grocery-container">
+                <List items={["a", "b", "c"]}/>
+            </div>
+        </section>
+    )
 }
 
 export default App
