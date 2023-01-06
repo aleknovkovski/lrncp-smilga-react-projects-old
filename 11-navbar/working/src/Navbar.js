c3dbc3ea7a16205f0665d8bd113ff3b52b4860cd
Alek Novkovski <alek@greatcode2.com>
2023-01-07 00:01:52 +0100

    11: Update menu height on links toggle

--- 11-navbar/working/src/Navbar.js
+++ 11-navbar/working/src/Navbar.js
@@ -5,9 +5,20 @@ import logo from './logo.svg'
 
 const Navbar = () => {
     const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
     const toggleLinks = () => {
         setShowLinks(!showLinks);
     };
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if (showLinks) {
+            linksContainerRef.current.style.height = `${linksHeight}px`;
+        } else {
+            linksContainerRef.current.style.height = '0px';
+        }
+    }, [showLinks]);
 
     return (
         <nav>
@@ -18,8 +29,8 @@ const Navbar = () => {
                         <FaBars/>
                     </button>
                 </div>
-                <div className='links-container show-container'>
-                    <ul className="links">
+                <div className='links-container' ref={linksContainerRef}>
+                    <ul className='links' ref={linksRef}>
                         {links.map((link) => {
                             const {id, url, text} = link;
                             return (
