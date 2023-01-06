763ddabbd72a6f7c5013022b399557d490b84ea8
Alek Novkovski <alek@greatcode2.com>
2023-01-06 14:10:49 +0100

    10: Setup conditional display of Alert

--- 10-grocery-bud/working/src/App.js
+++ 10-grocery-bud/working/src/App.js
@@ -3,9 +3,12 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
+    const [alerting, setAlerting] = useState(true)
+
     return (
         <section className="section-center">
             <form className="grocery-form">
+                {alerting ? <Alert/> : null}
                 <h3>grocery bud</h3>
                 <div className="form-control">
                     <input type="text" className="grocery" placeholder="e.g. eggs"/>
