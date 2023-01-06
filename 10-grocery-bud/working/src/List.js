e4697cb44e1ac17b1ce0f19ab77e8f53fd8a6785
Alek Novkovski <alek@greatcode2.com>
2023-01-06 14:04:44 +0100

    10: Add 'Clear Items' button

--- 10-grocery-bud/working/src/List.js
+++ 10-grocery-bud/working/src/List.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 const List = ({items}) => {
-  return (
-      items.map((item)=>{
+    const itemsMarkup = items.map((item)=>{
         return (
-        <article className="grocery-item">
-            <p className="title">{item}</p>
-        </article>
+            <article className="grocery-item">
+                <p className="title">{item}</p>
+            </article>
         )
-      })
-  )
+    })
+
+  return (<>
+      {itemsMarkup}
+      <button className="clear-btn">clear items</button>
+  </>)
 }
 
 export default List
