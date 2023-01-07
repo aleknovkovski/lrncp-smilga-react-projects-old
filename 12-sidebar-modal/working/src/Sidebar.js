6d5929f7af2b8e5f04214f06a44e5b0fdb799c1f
Alek Novkovski <alek@greatcode2.com>
2023-01-07 23:15:11 +0100

    12: Prettier reformat

--- 12-sidebar-modal/working/src/Sidebar.js
+++ 12-sidebar-modal/working/src/Sidebar.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import {FaTimes} from 'react-icons/fa'
+import {social, links} from './data'
 
 const Sidebar = () => {
-  return (
-      <aside className={`sidebar show-sidebar`}>
-        <div className='sidebar-header'>
-          <img src={logo} className='logo' alt='coding addict' />
-          <button className='close-btn'>
-            <FaTimes />
-          </button>
-        </div>
-      </aside>
-  )
+    return (
+        <aside className={`sidebar show-sidebar`}>
+            <div className='sidebar-header'>
+                <img src={logo} className='logo' alt='coding addict'/>
+                <button className='close-btn'>
+                    <FaTimes/>
+                </button>
+            </div>
+        </aside>
+    )
 }
 
 export default Sidebar
