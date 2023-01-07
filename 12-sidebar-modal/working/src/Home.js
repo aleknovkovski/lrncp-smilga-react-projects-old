561015b64bf00f022868b6fbfb151d5c09651922
Alek Novkovski <alek@greatcode2.com>
2023-01-07 23:52:17 +0100

    12: Prettier Reformat

--- 12-sidebar-modal/working/src/Home.js
+++ 12-sidebar-modal/working/src/Home.js
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, {useContext} from 'react'
+import {FaBars} from 'react-icons/fa'
 
 const Home = () => {
-  return (
-      <main>
-        <button className='sidebar-toggle'>
-          <FaBars />
-        </button>
-        <button className='btn'>
-          show modal
-        </button>
-      </main>
-  );
+    return (
+        <main>
+            <button className='sidebar-toggle'>
+                <FaBars/>
+            </button>
+            <button className='btn'>
+                show modal
+            </button>
+        </main>
+    );
 }
 
 export default Home
