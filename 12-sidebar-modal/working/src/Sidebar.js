8506ebfb486c03c5915da9d63fa8df15043b337c
Alek Novkovski <alek@greatcode2.com>
2023-01-07 22:30:14 +0100

    12: Build out markup for sidebar header

--- 12-sidebar-modal/working/src/Sidebar.js
+++ 12-sidebar-modal/working/src/Sidebar.js
@@ -4,7 +4,16 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+      <aside className={`sidebar show-sidebar`}>
+        <div className='sidebar-header'>
+          <img src={logo} className='logo' alt='coding addict' />
+          <button className='close-btn'>
+            <FaTimes />
+          </button>
+        </div>
+      </aside>
+  )
 }
 
 export default Sidebar
