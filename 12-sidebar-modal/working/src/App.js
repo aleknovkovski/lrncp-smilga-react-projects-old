c541342f7de5b1c9d00660ad650d95e754dfd0bc
Alek Novkovski <alek@greatcode2.com>
2023-01-07 21:50:01 +0100

    12: Prettier reformat app.js

--- 12-sidebar-modal/working/src/App.js
+++ 12-sidebar-modal/working/src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
+
 function App() {
-  return (
-    <>
-      <h2>modal-sidebar project setup</h2>
-    </>
-  )
+    return (
+        <>
+            <h2>modal-sidebar project setup</h2>
+        </>
+    )
 }
 
 export default App
