7899b5950f7ae4b5e8a94129e9d8c7bf3c560435
Alek Novkovski <alek@greatcode2.com>
2023-01-08 00:01:45 +0100

    12: Console log context value in Home.js

--- 12-sidebar-modal/working/src/Home.js
+++ 12-sidebar-modal/working/src/Home.js
@@ -1,7 +1,10 @@
 import React, {useContext} from 'react'
 import {FaBars} from 'react-icons/fa'
+import {AppContext} from "./context";
 
 const Home = () => {
+    const data = useContext(AppContext)
+    console.log(data)
     return (
         <main>
             <button className='sidebar-toggle'>
