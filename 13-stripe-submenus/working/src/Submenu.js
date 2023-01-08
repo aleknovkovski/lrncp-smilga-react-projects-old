1210314e82b342fe1218502a8aa12f0d55838007
Alek Novkovski <alek@greatcode2.com>
2023-01-08 16:03:32 +0100

    13: Add container element for submenu markup; show conditionally

--- 13-stripe-submenus/working/src/Submenu.js
+++ 13-stripe-submenus/working/src/Submenu.js
@@ -4,7 +4,11 @@ import {useGlobalContext} from "./context";
 const Submenu = () => {
   const {isSubmenuOpen} = useGlobalContext()
 
-  return <h2>submenu component</h2>
+  return (
+      <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
+          submenu
+      </aside>
+  )
 }
 
 export default Submenu
