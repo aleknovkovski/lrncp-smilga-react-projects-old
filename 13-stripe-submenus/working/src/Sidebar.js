507ef113d01791e53fee57b68257b06a8d112ca2
Alek Novkovski <alek@greatcode2.com>
2023-01-08 14:54:37 +0100

    13: Import sideBarOpen state, closeSidebar fn from Context

--- 13-stripe-submenus/working/src/Sidebar.js
+++ 13-stripe-submenus/working/src/Sidebar.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import {useGlobalContext} from "./context";
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+
   return <h2>sidebar component</h2>
 }
 
