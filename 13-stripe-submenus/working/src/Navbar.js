3b89aa0255684c35291ffe86142d59761ab36882
Alek Novkovski <alek@greatcode2.com>
2023-01-08 14:02:14 +0100

    13: Add the 3 menu items in Navbar

--- 13-stripe-submenus/working/src/Navbar.js
+++ 13-stripe-submenus/working/src/Navbar.js
@@ -15,6 +15,23 @@ const Navbar = () => {
               <FaBars />
             </button>
           </div>
+          <ul className='nav-links'>
+            <li>
+              <button className='link-btn'>
+                products
+              </button>
+            </li>
+            <li>
+              <button className='link-btn'>
+                developers
+              </button>
+            </li>
+            <li>
+              <button className='link-btn'>
+                company
+              </button>
+            </li>
+          </ul>
         </div>
       </nav>
   );
