ef878ca5347880dfaa41743dc161b96a6413d2a2
Alek Novkovski <alek@greatcode2.com>
2023-01-08 10:13:05 +0100

    12: In context, create fns to open/close modal,sidebar

--- 12-sidebar-modal/working/src/context.js
+++ 12-sidebar-modal/working/src/context.js
@@ -6,6 +6,20 @@ export function AppProvider({children}) {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    };
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <AppContext.Provider value="hello">
             {children}
