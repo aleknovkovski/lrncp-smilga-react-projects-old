6fbae70ded469d0cc7fbbf570833b639a8089a76
Alek Novkovski <alek@greatcode2.com>
2023-01-08 22:51:44 +0100

    14: Setup clearCart fn, use in CartContainer

--- 14-cart/working/src/context.js
+++ 14-cart/working/src/context.js
@@ -16,8 +16,12 @@ const initialState = {
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
+    function clearCart() {
+        dispatch({type: 'CLEAR_CART'})
+    }
+
     return (
-        <AppContext.Provider value={{...state,}}>
+        <AppContext.Provider value={{...state, clearCart}}>
             {children}
         </AppContext.Provider>
     )
