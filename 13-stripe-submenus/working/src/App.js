ea84dee1e1cb65fdd10168053048a817ae05c067
Alek Novkovski <alek@greatcode2.com>
2023-01-08 12:27:56 +0100

    13: Return all top-level components in app.js

--- 13-stripe-submenus/working/src/App.js
+++ 13-stripe-submenus/working/src/App.js
@@ -6,9 +6,12 @@ import Submenu from './Submenu'
 function App() {
     return (
         <>
-            <h2>stripe submenus setup</h2>
+            <Navbar />
+            <Sidebar />
+            <Hero />
+            <Submenu />
         </>
-    )
+    );
 }
 
 export default App
