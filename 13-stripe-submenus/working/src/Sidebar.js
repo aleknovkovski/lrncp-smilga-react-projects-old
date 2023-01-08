9c69c9e5c90bca303479da3c2ff89a0c7a493cb4
Alek Novkovski <alek@greatcode2.com>
2023-01-08 15:02:01 +0100

    13: On click, closeSidebar, set default as not opened

--- 13-stripe-submenus/working/src/Sidebar.js
+++ 13-stripe-submenus/working/src/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
   return (
       <div className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'}`}>
           <aside className='sidebar'>
-              <button className='close-btn'>
+              <button className='close-btn' onClick={closeSidebar}>
                   <FaTimes />
               </button>
           </aside>
--- 13-stripe-submenus/working/src/context.js
+++ 13-stripe-submenus/working/src/context.js
@@ -4,7 +4,7 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 export function AppProvider({children}) {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
 
     const openSidebar = () => {
