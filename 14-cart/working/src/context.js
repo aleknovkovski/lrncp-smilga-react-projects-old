e03bd19d14923b02b6980cf845b30075776055a0
Alek Novkovski <alek@greatcode2.com>
2023-01-08 22:32:20 +0100

    14: Refactor useState into useReducer

--- 14-cart/working/src/context.js
+++ 14-cart/working/src/context.js
@@ -6,15 +6,18 @@ import reducer from './reducer'
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
+const initialState = {
+    loading: false,
+    cart: cartItems,
+    total: 0,
+    amount: 0,
+}
+
 const AppProvider = ({children}) => {
-    const [cart, setCart] = useState(cartItems)
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
-        <AppContext.Provider
-            value={{
-                cart,
-            }}
-        >
+        <AppContext.Provider value={{...state,}}>
             {children}
         </AppContext.Provider>
     )
