0fc7a29e074866258f5457097971036a6f78d024
Alek Novkovski <alek@greatcode2.com>
2023-01-08 16:50:34 +0100

    13: Prettier reformat

--- 13-stripe-submenus/working/src/Submenu.js
+++ 13-stripe-submenus/working/src/Submenu.js
@@ -1,14 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import {useGlobalContext} from "./context";
 
 const Submenu = () => {
-  const {isSubmenuOpen} = useGlobalContext()
-
-  return (
-      <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
-          submenu
-      </aside>
-  )
+    const {isSubmenuOpen, location} = useGlobalContext()
+    return (
+        <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
+            submenu
+        </aside>
+    )
 }
 
 export default Submenu
