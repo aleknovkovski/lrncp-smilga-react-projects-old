2be51147768cf4eee1a5896bce8b17a7cbda4059
Alek Novkovski <alek@greatcode2.com>
2023-01-08 16:56:07 +0100

    13: Set submenu css location w passed values

--- 13-stripe-submenus/working/src/Submenu.js
+++ 13-stripe-submenus/working/src/Submenu.js
@@ -7,6 +7,10 @@ const Submenu = () => {
 
     useEffect(()=>{
         console.log(location)
+        const submenu = container.current
+        const {center,bottom} = location;
+        submenu.style.left = `${center}px`
+        submenu.style.top = `${bottom}px`
     },[location])
 
     return (
