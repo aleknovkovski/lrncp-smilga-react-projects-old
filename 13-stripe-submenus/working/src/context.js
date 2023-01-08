022b285768fea15dcab853df587e2ab7092eec42
Alek Novkovski <alek@greatcode2.com>
2023-01-08 12:30:40 +0100

    13: Setup AppProvider, GlobalContext in context.js

--- 13-stripe-submenus/working/src/context.js
+++ 13-stripe-submenus/working/src/context.js
@@ -1,2 +1,16 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export function AppProvider({children}) {
+    return (
+        <AppContext.Provider>
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+};
