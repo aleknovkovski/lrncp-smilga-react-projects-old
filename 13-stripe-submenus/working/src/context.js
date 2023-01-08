65c2d268816912d6296ff12be20e6716fdcd38f5
Alek Novkovski <alek@greatcode2.com>
2023-01-08 13:15:14 +0100

    13: Setup sidebar/submenu open-state tracking

--- 13-stripe-submenus/working/src/context.js
+++ 13-stripe-submenus/working/src/context.js
@@ -4,6 +4,9 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 export function AppProvider({children}) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
     return (
         <AppContext.Provider>
             {children}
