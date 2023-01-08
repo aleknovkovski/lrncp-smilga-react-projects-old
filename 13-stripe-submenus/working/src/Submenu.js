bd0e1c36661de178f37ddec95756fb70935f0c83
Alek Novkovski <alek@greatcode2.com>
2023-01-08 16:52:55 +0100

    13: Asign a ref to submenu container element

--- 13-stripe-submenus/working/src/Submenu.js
+++ 13-stripe-submenus/working/src/Submenu.js
@@ -3,12 +3,14 @@ import {useGlobalContext} from "./context";
 
 const Submenu = () => {
     const {isSubmenuOpen, location} = useGlobalContext()
+    const container = useRef(null)
+
     useEffect(()=>{
         console.log(location)
     },[location])
 
     return (
-        <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
+        <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}  ref={container}>
             submenu
         </aside>
     )
