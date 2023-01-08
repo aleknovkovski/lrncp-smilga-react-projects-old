359d67d33d08b4b5d0be2100d2708038b5814781
Alek Novkovski <alek@greatcode2.com>
2023-01-08 13:57:52 +0100

    13: Import relevant fns in Navbar.js

--- 13-stripe-submenus/working/src/Navbar.js
+++ 13-stripe-submenus/working/src/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import {useGlobalContext} from "./context";
 
 const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
   return <h2>navbar component</h2>
 }
 
