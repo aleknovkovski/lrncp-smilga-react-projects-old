6ea13310c7976af1f614de06d87f7e8532ffaed0
Alek Novkovski <alek@greatcode2.com>
2023-01-08 16:51:32 +0100

    13: Setup useEffect for each time location changes

--- 13-stripe-submenus/working/src/Submenu.js
+++ 13-stripe-submenus/working/src/Submenu.js
@@ -3,6 +3,10 @@ import {useGlobalContext} from "./context";
 
 const Submenu = () => {
     const {isSubmenuOpen, location} = useGlobalContext()
+    useEffect(()=>{
+        console.log(location)
+    },[location])
+
     return (
         <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
             submenu
